6ccda3e5ac15a789358cb98b29525fd55f5e7d24
SallyPY <sallypyuen@gmail.com>
2014-11-15 19:05:15 -0500

    fix grid of ImageSpacePanel
    fix size to 2 rows and 4 columns
    add a border

--- Escape/src/escape/ImageSpacePanel.java
+++ Escape/src/escape/ImageSpacePanel.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
 
+import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -21,16 +22,18 @@ public class ImageSpacePanel extends JPanel{
 		super();
 		
 	  
-		setLayout(new GridLayout(4,2));
+		setLayout(new GridLayout(2,4));
 		setBackground(Color.BLUE);
 
 		JButton p = new JButton("hi");
 		
-		panelHolder = new JPanel[4][2];    
+		panelHolder = new JPanel[2][4];    
 
-		for(int m = 0; m < 4; m++) {
-		   for(int n = 0; n < 2; n++) {
+		for(int m = 0; m < 2; m++) {
+		   for(int n = 0; n < 4; n++) {
 		      panelHolder[m][n] = new JPanel();
+		      panelHolder[m][n].setPreferredSize(new Dimension(40,40));
+		      panelHolder[m][n].setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		      add(panelHolder[m][n]);
 		   }
 		}
